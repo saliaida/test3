70987556d71506d9c02bb6cb6aa16a0e9a38dbf4
django1401 <126799693+django1401@users.noreply.github.com>
2023-12-29 17:12:00 +0330

    create file

---
+++ myfile.py
@@ -0,0 +1,45 @@
+
+
+
+class Person:
+    a = 1
+    _b = 2
+    __c = 3
+    def __init__(self):
+        self.score1 = 10
+        self._score2 = 20
+        self.__score3 = 30
+        
+class Student(Person):
+    def __init__(self,fname,lname,age):
+        Person.__init__(self)
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+    def __str__(self):
+        # lst = [self.fname,self.lname,self.age]
+        return f'-{self.fname}\t{self.lname}\n\t{self.age}'
+    def __add__(self, other):
+        return self.age + other.age
+
+
+s1 = Student('ali', 'ahmadi', 20)
+s2 = Student('reza', 'rezayee', 18)
+
+print(s1.__add__(s2))
+print(s1 + s2)
+print(s1 * s2)
+
+print(s1)
+
+
+
+
+# def _func():
+#     print('*********')
+
+# def __func():
+#     print('============')
+
+
+
